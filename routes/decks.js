6590e8a03b4079ff3f13fe7cb278bb4b3360ee85
Seth Vargas <shvargas01@gmail.com>
2023-09-30 01:56:34 -0500

    Set up basics for deck routes

---
+++ routes/decks.js
@@ -0,0 +1,78 @@
+/* routes for decks */
+
+const express = require("express");
+const Deck = require("../models/deck");
+const { NotFoundError } = require("../expressError");
+const router = new express.Router();
+
+/* Returns a list of decks
+  - optional filters: username, isPublic, orderBy
+  
+  Authorization: Admin / logged in user
+*/
+
+router.get("/", async function (req, res, next) {
+  const query = req.query;
+
+  try {
+    const decks = await Deck.getAll(query);
+    return res.json({ decks });
+  } catch (error) {
+    return next(error);
+  }
+});
+
+/* Returns JSON for a deck based on a username
+  - if user not found, throws 404 error
+  
+  Authorization: Admin / logged in user only
+*/
+
+router.get("/:id", async function (req, res, next) {
+  const id = req.params.id;
+
+  try {
+    const deck = await Deck.get(id);
+
+    if (!deck) throw new NotFoundError(`No deck: ${id}`);
+
+    return res.json({ deck });
+  } catch (error) {
+    return next(error);
+  }
+});
+
+/* Adds new deck to db and returns JSON with new deck data
+  - If deck exists, throw 500 error
+  
+  Authorization: Admin / logged in user
+*/
+
+router.post("/", async function (req, res, next) {
+  try {
+    const deck = await Deck.create(req.body);
+    return res.status(201).json({ deck });
+  } catch (error) {
+    return next(error);
+  }
+});
+
+// TODO: PATCH /:id
+
+/* Removes deck from db and returns JSON with id
+  - If no deck, nothing happens
+  
+  Authorization: Admin, logged in user
+*/
+
+router.delete("/:id", async function (req, res, next) {
+  const id = req.params.id;
+  try {
+    await Deck.remove(id);
+    return res.json({ deleted: id });
+  } catch (error) {
+    return next(error);
+  }
+});
+
+module.exports = router;
