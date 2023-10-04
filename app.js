e84e7ba286ba9d2ff651d03b6eb24883ac04830e
Seth Vargas <shvargas01@gmail.com>
2023-10-03 23:00:13 -0500

    Condensed deck routes into user routes

--- app.js
+++ app.js
@@ -6,15 +6,16 @@ const { NotFoundError } = require("./expressError");
 // TODO - require routes here
 const cardRoutes = require("./routes/cards");
 const deckRoutes = require("./routes/decks");
-const userRoutes = require("./routes/users");
+const { userRouter } = require("./routes/users");
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
 app.use("/cards", cardRoutes);
-app.use("/decks", deckRoutes);
-app.use("/users", userRoutes);
+app.use("/users", userRouter);
+
+// app.use("/users/:username/decks", deckRoutes);
 
 app.use((req, res, next) => {
   return next(new NotFoundError());
