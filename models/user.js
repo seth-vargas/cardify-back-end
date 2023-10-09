24c34ebff5e6839a824fc699dfe30faf69c39061
Seth Vargas <shvargas01@gmail.com>
2023-10-09 00:00:32 -0500

    Extracted some user methods into other models

--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
 const client = require("../db");
 const { BadRequestError, NotFoundError } = require("../expressError");
 const Deck = require("./deck");
+const Favorite = require("./favorite");
+const Follow = require("./follow");
 
 /* Model for users */
 
@@ -76,9 +78,9 @@ class User {
     if (!user) throw new NotFoundError(`User not found: ${username}`);
 
     user.decks = await Deck.getAll(username, {});
-    user.followers = await User.getFollowers(user);
-    user.following = await User.getFollowing(user);
-    user.favorites = await User.getFavorites(user);
+    user.followers = await Follow.getFollowers(user);
+    user.following = await Follow.getFollowing(user);
+    user.favorites = await Favorite.get(user);
 
     return user;
   }
@@ -148,168 +150,6 @@ class User {
     const user = result.rows[0];
 
     if (!user) throw new NotFoundError(`No user username: ${username}`);
-  }
-
-  /* Get a list of users who follow user. */
-
-  static async getFollowers(user) {
-    const followsResult = await client.query(
-      `SELECT id, following_user_id AS "followingUserId", followed_user_id AS "followedUserId", created_at AS "createdAt"
-          FROM follows
-          WHERE followed_user_id = $1`,
-      [user.id]
-    );
-
-    return followsResult.rows;
-  }
-
-  /* Get a list of users who the user follows. */
-
-  static async getFollowing(user) {
-    const followingResult = await client.query(
-      `SELECT id, following_user_id AS "followingUserId", followed_user_id AS "followedUserId", created_at AS "createdAt"
-          FROM follows
-          WHERE following_user_id = $1`,
-      [user.id]
-    );
-
-    return followingResult.rows;
-  }
-
-  /* Follow another user and return success/error message
-    - user cannot follow themselves.
-    - if either user cannot be found, throw 404 error.
-    */
-
-  static async follow(follower, followed) {
-    // check follower is not followed
-    if (follower === followed)
-      throw new BadRequestError("User cannot follow themselves. Loser.");
-
-    // check if both users exist
-    const [followingUser, followedUser] = await Promise.all([
-      User.getOr404(follower),
-      User.getOr404(followed),
-    ]);
-
-    // check if followed user and following user already have a relationship
-    const followsResult = await client.query(
-      `SELECT id FROM follows WHERE followed_user_id = $1 AND following_user_id = $2`,
-      [followedUser.id, followingUser.id]
-    );
-
-    const follows = followsResult.rows[0];
-
-    if (follows)
-      throw new BadRequestError(
-        `${followingUser.username} is already following ${followedUser.username}.`
-      );
-
-    // create a relationship => {message: "<FOLLOWING_USERNAME> subscribed to <FOLLOWED_USERNAME>'s feed."}
-    const insertResult = await client.query(
-      `INSERT INTO follows (following_user_id, followed_user_id)
-      VALUES ($1, $2)
-      RETURNING id, following_user_id AS "followingUserId", followed_user_id AS "followedUserId", created_at AS "createdAt"`,
-      [followingUser.id, followedUser.id]
-    );
-
-    const result = insertResult.rows[0];
-    result.message = `${follower} subscribed to ${followed}'s feed.`;
-    return result;
-  }
-
-  /* Unfollow another user and return success/error message.
-    - User cannot unfollow someone who they do not follow.
-    - if either user cannot be found, throw 404 error.
-  */
-
-  static async unfollow(follower, followed) {
-    // check that both users exist
-    const [followingUser, followedUser] = await Promise.all([
-      User.getOr404(follower),
-      User.getOr404(followed),
-    ]);
-
-    // check if followed user and following user already have a relationship
-    const followsResult = await client.query(
-      `SELECT id FROM follows WHERE followed_user_id = $1 AND following_user_id = $2`,
-      [followedUser.id, followingUser.id]
-    );
-
-    const follows = followsResult.rows[0];
-
-    if (!follows)
-      throw new BadRequestError(
-        `${followingUser.username} is not following ${followedUser.username}`
-      );
-
-    const deleteResult = await client.query(
-      `DELETE
-      FROM follows
-      WHERE id = $1
-      RETURNING id`,
-      [follows.id]
-    );
-
-    return `${follower} successfully unfollowed ${followed}.`;
-  }
-
-  static async getFavorites(user) {
-    const favoritesResult = await client.query(
-      `SELECT id, user_id AS "userId", deck_id AS "deckId", created_at AS "createdAt"
-      FROM favorites
-      WHERE user_id = $1`,
-      [user.id]
-    );
-
-    return favoritesResult.rows;
-  }
-
-  static async addDeckToFavorites(username, deckSlug) {
-    const [user, deck] = await Promise.all([
-      User.getOr404(username),
-      Deck.getOr404(deckSlug),
-    ]);
-
-    const insertResult = await client.query(
-      `INSERT INTO favorites (user_id, deck_id)
-      VALUES ($1, $2)
-      RETURNING id, user_id AS "userId", deck_id AS "deckId", created_at AS "createdAt"`,
-      [user.id, deck.id]
-    );
-
-    const result = insertResult.rows[0];
-    result.message = `${username} added ${deck.title} to their favorites.`;
-
-    return result;
-  }
-
-  static async removeDeckFromFavorites(username, deckSlug) {
-    const [user, deck] = await Promise.all([
-      User.getOr404(username),
-      Deck.getOr404(deckSlug),
-    ]);
-
-    const favoritesResult = await client.query(
-      `SELECT id FROM favorites WHERE user_id = $1 AND deck_id = $2`,
-      [user.id, deck.id]
-    );
-
-    const favorites = favoritesResult.rows[0];
-
-    if (!favorites)
-      throw new BadRequestError(
-        `${user.username} has not favorited deck ${deck.title}`
-      );
-
-    const deleteResult = await client.query(
-      `DELETE FROM favorites
-      WHERE id = $1
-      RETURNING id`,
-      [favorites.id]
-    );
-
-    return `${username} successfully removed ${deck.title} from their favorites list.`;
   }
 }
 
