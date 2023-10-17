985b2a3e6160be6cac14da4c00e61afaf872c6d5
Seth Vargas <shvargas01@gmail.com>
2023-10-17 14:22:47 -0500

    Added bcrypt for authenticating

--- config.js
+++ config.js
@@ -3,7 +3,9 @@ require("colors");
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret";
 
-const PORT = process.env.PORT || 3000;
+const PORT = +process.env.PORT || 3000;
+
+const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
 
 function getDatabaseUri() {
   return process.env.NODE_ENV === "test"
@@ -16,11 +18,14 @@ console.log("------------------------------------".america);
 console.log("Config:".green);
 console.log("SECRET_KEY:".yellow, SECRET_KEY);
 console.log("PORT".yellow, PORT.toString());
+console.log("BCRYPT_WORK_FACTOR:".yellow, BCRYPT_WORK_FACTOR.toString());
+console.log("Database:".yellow, getDatabaseUri());
 console.log("------------------------------------".america);
 console.log("\n");
 
 module.exports = {
   SECRET_KEY,
   PORT,
+  BCRYPT_WORK_FACTOR,
   getDatabaseUri,
 };
