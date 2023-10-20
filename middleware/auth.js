866d95b3e08d833c94e5c89d0430cbffe1ef5e89
Seth Vargas <shvargas01@gmail.com>
2023-10-20 17:23:45 -0500

    Created authentication helper file

---
+++ middleware/auth.js
@@ -0,0 +1,66 @@
+/** Convenience middleware to handle common auth cases in routes. */
+
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../config");
+const { UnauthorizedError } = require("../expressError");
+
+/* If a token was provided, verify it, and, if valid, store the token payload. */
+
+function authenticateJWT(req, res, next) {
+  try {
+    const authHeader = req.headers && req.headers.authorization;
+    if (authHeader) {
+      const token = authHeader.replace(/^[Bb]earer /, "").trim();
+      res.locals.user = jwt.verify(token, SECRET_KEY);
+    }
+    return next();
+  } catch (err) {
+    return next();
+  }
+}
+
+/* Ensures that client must be logged in. */
+
+function ensureLoggedIn(req, res, next) {
+  try {
+    if (!res.locals.user) throw new UnauthorizedError();
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+}
+
+/* Ensures that the client is an admin. */
+
+function ensureAdmin(req, res, next) {
+  try {
+    if (!res.locals.user || !res.locals.user.isAdmin) {
+      throw new UnauthorizedError();
+    }
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+}
+
+/* Ensures that the clients username matches the username found in the URL. */
+
+function ensureCorrectUser(req, res, next) {
+  try {
+    const user = res.locals.user;
+
+    if (!user && user.username !== req.params.username)
+      throw new UnauthorizedError();
+
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+}
+
+module.exports = {
+  authenticateJWT,
+  ensureLoggedIn,
+  ensureAdmin,
+  ensureCorrectUser,
+};
