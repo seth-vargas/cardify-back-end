83d75a6fc6b8eb1708bc52ce54c5a350caf85e56
Seth Vargas <shvargas01@gmail.com>
2023-10-24 21:45:13 -0500

    Added authenticate routes. Put card routes back

--- app.js
+++ app.js
@@ -6,16 +6,19 @@ const { NotFoundError } = require("./expressError");
 const { authenticateJWT } = require("./middleware/auth");
 
 const authRoutes = require("./routes/auth");
+const cardRoutes = require("./routes/cards");
 const { deckRouter } = require("./routes/decks");
 const { userRouter } = require("./routes/users");
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 app.use(morgan("dev"));
+app.use(authenticateJWT);
 
 app.use("/auth", authRoutes);
+app.use("/cards", cardRoutes);
 app.use("/users", userRouter);
 app.use("/decks", deckRouter);
 
